ff2abaadab88fc74e0e030e9411ba00dc820794a
tomashbaguiar <tomashbaguiar@ufmg.br>
2017-01-29 11:58:16 -0200

    Primeira versão do protocolo de janela deslizante

---
+++ slid_protocol.c
@@ -0,0 +1,4 @@
+#include "slid_protocol.h"
+
+typedef struct window SWHeader;                                                                 // Define o nome da estrutura do cabeçalho do protocolo.
+
---
+++ slid_protocol.h
@@ -0,0 +1,10 @@
+#ifndef SLID_PROTOCOL_H
+    #define SLID_PROTOCOL_H
+
+    struct window   {
+        //int 
+        //int
+        //int
+    };
+
+#endif /* SLID_PROTOCOL_H */
